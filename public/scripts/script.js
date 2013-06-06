a7b2d3271579d97ac67e688e76a509bb0200cd1b
Will Anderson <will@itsananderson.com>
2013-06-06 16:25:28 -0700

    Cleanup etc

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,15 +1,4 @@
 var app = angular.module('app', [])
-    
-	.config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/agencies', { templateUrl: 'partials/agencies.html', controller: 'AgencyCtrl' });
-        $routeProvider.when('/advertisers', { templateUrl: 'partials/advertisers.html', controller: 'AdvertiserCtrl' });
-        $routeProvider.when('/engagements', { templateUrl: 'partials/engagements.html', controller: 'EngagementCtrl' });
-        $routeProvider.otherwise({ redirectTo: '/agencies' });
-    }])
-	
-	.controller('MdCtrl', function ($scope) {
-		
-	})
 	
 	.filter('markdown', function() {
 		return function(text) {
